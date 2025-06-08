497ac8eb57bd2d2f3bd42a2a4cded533e4f2b767
suzuking19 <suzuki.coding.19@gmail.com>
2025-06-08 11:23:20 +0900

    generateBuildIdを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  generateBuildId: async () => {
+    return Date.now().toString();
+  },
 };
 
 export default nextConfig;
